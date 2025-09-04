9b131b74a0b9356cf1b11410c23864a08826fcb2
worldofhope <n.sadova98@gmail.com>
2025-09-04 18:26:51 +0500

    Update scss.js

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,42 +1,34 @@
+// gulp/tasks/scss.js
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
-
 import cleanCss from 'gulp-clean-css';
-// import webpcss from 'gulp-webpcss';
 import autoprefixer from 'gulp-autoprefixer';
-import groupCssMediaQueries from 'gulp-group-css-media-queries';
+// import groupCssMediaQueries from 'gulp-group-css-media-queries'; // временно выключим
+import gulpIf from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 const sass = gulpSass(dartSass);
+const isProd = process.env.NODE_ENV === 'production';
+const isCI = !!process.env.CI;
 
 export const scss = () => {
-  return app.gulp.src(app.path.src.scss, { sourcemaps: true })
-    .pipe(app.plugins.plumber(
-      app.plugins.notify.onError({
-          title: "SCSS",
-          message: "Error: <%= error.message %>"
-      })))
+  return app.gulp.src(app.path.src.scss, { sourcemaps: !isProd })
+    // в CI лучше без notify, чтобы не падать на notify-send
+    .pipe(app.plugins.plumber(isCI ? undefined : app.plugins.notify.onError({
+      title: 'SCSS',
+      message: 'Error: <%= error.message %>'
+    })))
     .pipe(app.plugins.replace(/@img\//g, '../img/'))
-    .pipe(sass({
-        outputStyle: 'expanded',
+    .pipe(sass.sync({ outputStyle: 'expanded' }).on('error', function(err) {
+      console.error('[SASS ERROR]\n', err.formatted || err.message);
+      this.emit('end'); // чтобы gulp показал лог и не «умирал» молча
     }))
-    .pipe(groupCssMediaQueries())
-    // Расскоментировать если нужна автоматическая замена webp изображений 
-    // .pipe(webpcss({
-    //     webpClass: ".webp",
-    //     noWebpClass: ".no-webp"
-    // }))
-    .pipe(autoprefixer({
-      grid: true,
-      overrideBrowserList: ["last 3 versions"],
-      cascade: true,
-    }))
-    // Расскоментировать если нужен не сжатый файл css
+    // .pipe(groupCssMediaQueries()) // ← отключено для диагностики
+    .pipe(autoprefixer({ grid: true, overrideBrowserslist: ['last 3 versions'], cascade: false }))
     .pipe(app.gulp.dest(app.path.build.css))
     .pipe(cleanCss())
-    .pipe(rename({
-      extname: ".min.css"
-    }))
-    .pipe(app.gulp.dest(app.path.build.css))
-    .pipe(app.plugins.browsersync.stream());
-}
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(app.gulp.dest(app.path.build.css));
+    // .pipe(app.plugins.browsersync.stream()) // в CI не нужен
+};
